c2444c23ff2f5f32356e84692556677c39cde6f1
ALONEPAY <rapeepat.tk003@gmail.com>
2025-09-28 12:30:22 +0700

    สร้าง01/02/03 js

---
+++ JS01.js
@@ -0,0 +1,27 @@
+console.log(`hello`)
+console.log("AAAAA")
+console.log('ALOOO')
+
+console.log(1111)
+console.log(15.426879)
+
+console.log(true)
+console.log(false)
+
+console.log(null)
+
+console.log({
+    name : 'Sombat',
+    age : 20,
+    address : {
+        province : 'nakornpatom',
+        contry : 'Thailand'
+    },
+    food : ['pizz, kfc, rice']
+})
+
+console.log([1,2,3,4,5])
+console.log([111, 'Somjai', true, {name : 'Sombat', age : 20}])
+
+let X
+console.log(X)
---
+++ JS03.js
@@ -0,0 +1,32 @@
+console.log(20 % 2)
+console.log(2 % 20)
+console.log(0 % 500)
+
+console.log(111 == '111')
+
+console.log(111 == '111')
+
+console.log('sombat' < 'somjai')
+console.log('bangna' >= 'bangpoo')
+
+console.log(!true)
+console.log(!false)
+console.log(true && true)
+console.log(true && true)
+console.log(false && false)
+console.log(false && false)
+
+console.log(false || false)
+console.log(false || true)
+console.log(true || false)
+console.log(true || true)
+
+let datal = 100 % 30 == '0' ? 'wow...' : 'Haaa...'
+console.log(datal)
+
+let data2 = 1
+console.log(data2)
+data2++
+++data2
+data2++
+console.log(data2)
